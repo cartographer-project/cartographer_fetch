1c6fd552ecdf5921b22d853436296187d156836e
Damon Kohler <damonkohler@google.com>
2017-01-11 10:50:09 +0100

    Initial import.

---
+++ cartographer_fetch/configuration_files/freight.lua
@@ -0,0 +1,44 @@
+-- Copyright 2016 The Cartographer Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+include "map_builder.lua"
+
+options = {
+  map_builder = MAP_BUILDER,
+  map_frame = "map",
+  tracking_frame = "base_link",
+  published_frame = "odom",
+  odom_frame = "odom",
+  provide_odom_frame = false,
+  use_odometry = true,
+  use_laser_scan = true,
+  use_multi_echo_laser_scan = false,
+  num_point_clouds = 0,
+  lookup_transform_timeout_sec = 0.1,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+}
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+
+TRAJECTORY_BUILDER_2D.laser_min_range = 0.
+TRAJECTORY_BUILDER_2D.laser_max_range = 20.
+TRAJECTORY_BUILDER_2D.laser_missing_echo_ray_length = 5.
+-- TODO(damonkohler): Enable the IMU for outside of simulation.
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+
+SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 1e2
+
+return options
