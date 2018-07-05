81f69e84069bfda7c77fa41653c829ae667bf247
Christoph Schütte <cschuet@google.com>
2018-07-05 16:34:46 +0200

    Use new pure localization trimmer options. (#42)

--- cartographer_fetch/configuration_files/freight_localization.lua
+++ cartographer_fetch/configuration_files/freight_localization.lua
@@ -14,7 +14,9 @@
 
 include "freight.lua"
 
-TRAJECTORY_BUILDER.pure_localization = true
+TRAJECTORY_BUILDER.pure_localization_trimmer = {
+  max_submaps_to_keep = 3,
+}
 POSE_GRAPH.optimize_every_n_nodes = 10
 
 return options
